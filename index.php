988068bf47982906c45a335f3d883e12b88fd635
Eleanor Morgan <eleanorlouise99@gmail.com>
2020-01-10 12:57:26 +0000

    commit

--- index.php
+++ index.php
@@ -14,44 +14,7 @@
 
 <body>
     <!--Navigation-->
-    <nav>
-        <ul class="navList">
-            <li class="navItem">
-                <a href="http://www.eleanormorgan.co.uk/index.php">HOME</a>
-            </li>
-            <li class="navItem">
-                <div class="dropdown">
-                    <button onClick="dropdownMenu()" class="dropbtn">WORK</button>
-                    <div id="myDropdown" class="dropdown-content">
-                        <a href="http://www.eleanormorgan.co.uk/web-designs.php">Web designs</a>
-                        <a href="http://www.eleanormorgan.co.uk/ui-designs.php">UI designs</a>
-                        <a href="http://www.eleanormorgan.co.uk/graphic-designs.php">Graphic designs</a>
-                    </div>
-                </div>
-            </li>
-            <li class="navItem">
-                <a href="http://www.eleanormorgan.co.uk/about.php">ABOUT</a>
-            </li>
-            <li class="navItem">
-                <a href="http://www.eleanormorgan.co.uk/contact.php">CONTACT</a>
-            </li>
-        </ul>
-
-        <div class="mobileNav">
-            <div id="myLinks">
-              <a href="#">Home</a>
-              <a href="#">About</a>
-              <a href="#">Web designs</a>
-              <a href="#">UI designs</a>
-              <a href="#">Graphic designs</a>
-              <a href="#">Contact</a>
-            </div>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-              <i class="fa fa-bars" id="burger"></i>
-            </a>
-          </div>
-
-    </nav>
+    <?php include "nav.php";?>
 
     <!--Main heading/Opening text-->
     <div class="openingTxt">
---
+++ nav.php
@@ -0,0 +1,38 @@
+<nav>
+        <ul class="navList">
+            <li class="navItem">
+                <a href="http://www.eleanormorgan.co.uk/index.php">HOME</a>
+            </li>
+            <li class="navItem">
+                <div class="dropdown">
+                    <button onClick="dropdownMenu()" class="dropbtn">WORK</button>
+                    <div id="myDropdown" class="dropdown-content">
+                        <a href="http://www.eleanormorgan.co.uk/web-designs.php">Web designs</a>
+                        <a href="http://www.eleanormorgan.co.uk/ui-designs.php">UI designs</a>
+                        <a href="http://www.eleanormorgan.co.uk/graphic-designs.php">Graphic designs</a>
+                    </div>
+                </div>
+            </li>
+            <li class="navItem">
+                <a href="http://www.eleanormorgan.co.uk/about.php">ABOUT</a>
+            </li>
+            <li class="navItem">
+                <a href="http://www.eleanormorgan.co.uk/contact.php">CONTACT</a>
+            </li>
+        </ul>
+
+        <div class="mobileNav">
+            <div id="myLinks">
+              <a href="http://www.eleanormorgan.co.uk/index.php">Home</a>
+              <a href="http://www.eleanormorgan.co.uk/web-designs.php">Web designs</a>
+              <a href="http://www.eleanormorgan.co.ukui-designs.php">UI designs</a>
+              <a href="http://www.eleanormorgan.co.uk/graphic-designs.php">Graphic designs</a>
+              <a href="http://www.eleanormorgan.co.uk/about.php">About</a>
+              <a href="http://www.eleanormorgan.co.uk/contact.php">Contact</a>
+            </div>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+              <i class="fa fa-bars" id="burger"></i>
+            </a>
+          </div>
+
+    </nav>
